0ac9f31f4016d869ff427b3732d8db91c6cdc72d
edeeb4 <liza61198@gmail.com>
2026-07-07 16:31:09 -0400

    Use deployed backend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
-
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 function App() {
   //state management
   const [teams, setTeams] = useState([]); //list of avaialable teams for backend
